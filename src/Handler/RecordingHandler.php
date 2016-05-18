74f20e2d7063056e2edd10760fd36dcaec4865c9
jan arnold <jan.arnold@movingimage24.com>
2016-05-18 09:51:27 +0200

    Refactoring * use Interfaces

--- src/Handler/RecordingHandler.php
+++ src/Handler/RecordingHandler.php
@@ -2,9 +2,6 @@
 
 namespace Mi\Bundle\WowzaGuzzleClientBundle\Handler;
 
-use GuzzleHttp\Client;
-use Mi\Bundle\WowzaGuzzleClientBundle\Helper\RecordingHelper;
-use Mi\Bundle\WowzaGuzzleClientBundle\WowzaApiClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
@@ -13,31 +10,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  *
  * @codeCoverageIgnore
  */
-class RecordingHandler extends WowzaApiClient
+interface RecordingHandler
 {
-    /**@var RecordingHelper $recordingHelper */
-    private $recordingHelper;
-    private $data = [];
-
-    /**
-     * @param array           $wowzaConfig
-     * @param Client          $client
-     * @param RecordingHelper $recordingHelper
-     */
-    public function __construct(array $wowzaConfig, Client $client, RecordingHelper $recordingHelper)
-    {
-        parent::__construct($wowzaConfig, $client);
-
-        $this->recordingHelper = $recordingHelper;
-        $this->data            = [
-            'wowzaAdmin'         => $this->wowzaAdmin,
-            'wowzaAdminPassword' => $this->wowzaAdminPassword,
-            'wowzaProtocol'      => $this->wowzaProtocol,
-            'wowzaHostname'      => $this->wowzaHostname,
-            'wowzaDvrPort'       => $this->wowzaDvrPort,
-            'wowzaApp'           => $this->wowzaApp
-        ];
-    }
 
     /**
      * @param string $streamname
@@ -45,12 +19,7 @@ class RecordingHandler extends WowzaApiClient
      *
      * @return JsonResponse
      */
-    public function startRecording($streamname, $option = 'append')
-    {
-        $this->data['action'] = 'startRecording';
-
-        return $this->recordingTask($streamname, $option);
-    }
+    public function startRecording($streamname, $option = 'append');
 
     /**
      * @param string $streamname
@@ -58,28 +27,5 @@ class RecordingHandler extends WowzaApiClient
      *
      * @return JsonResponse
      */
-    public function stopRecording($streamname, $option = 'append')
-    {
-        $this->data['action'] = 'stopRecording';
-
-        return $this->recordingTask($streamname, $option);
-    }
-
-    /**
-     * @param string $streamname
-     * @param string $option
-     *
-     * @return JsonResponse
-     */
-    private function recordingTask($streamname, $option)
-    {
-        $this->data['streamname'] = $streamname;
-        $this->data['option']     = $option;
-
-        $url            = $this->recordingHelper->buildUrl('livestreamrecord', $this->data);
-        $result         = $this->recordingHelper->call($this->data, $url, $this->client);
-        $parsedResponse = $this->recordingHelper->parseResponse($result, $this->data);
-
-        return new JsonResponse($parsedResponse, $parsedResponse['code']);
-    }
+    public function stopRecording($streamname, $option = 'append');
 }
---
+++ src/Handler/WowzaCuepointHandler.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Mi\Bundle\WowzaGuzzleClientBundle\Handler;
+
+use GuzzleHttp\Client;
+use Mi\Bundle\WowzaGuzzleClientBundle\Helper\CuepointHelper;
+use Mi\Bundle\WowzaGuzzleClientBundle\WowzaApiClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+/**
+ * @author Jan Arnold <jan.arnold@movingimage.com>
+ *
+ * @codeCoverageIgnore
+ */
+class WowzaCuepointHandler extends WowzaApiClient implements CuepointHandler
+{
+    /**@var CuepointHelper $cuepointHelper */
+    private $cuepointHelper;
+    private $data = [];
+
+    /**
+     * @param array          $wowzaConfig
+     * @param Client         $client
+     * @param CuepointHelper $cuepointHelper
+     */
+    public function __construct(array $wowzaConfig, Client $client, CuepointHelper $cuepointHelper)
+    {
+        parent::__construct($wowzaConfig, $client);
+
+        $this->cuepointHelper = $cuepointHelper;
+        $this->data           = [
+            'wowzaAdmin'         => $this->wowzaAdmin,
+            'wowzaAdminPassword' => $this->wowzaAdminPassword,
+            'wowzaProtocol'      => $this->wowzaProtocol,
+            'wowzaHostname'      => $this->wowzaHostname,
+            'wowzaDvrPort'       => $this->wowzaDvrPort,
+            'wowzaApp'           => $this->wowzaApp
+        ];
+    }
+
+    /**
+     * @param string $streamname
+     * @param string $text
+     *
+     * @return JsonResponse
+     */
+    public function insertCuepoint($streamname, $text)
+    {
+        $this->data['action']     = 'cuepoint';
+        $this->data['streamname'] = $streamname;
+        $this->data['text']       = $text;
+
+        $url            = $this->cuepointHelper->buildUrl('livesetmetadata', $this->data);
+        $result         = $this->cuepointHelper->call($this->data, $url, $this->client);
+        $parsedResponse = $this->cuepointHelper->parseResponse($result, $this->data);
+
+        return new JsonResponse($parsedResponse, $parsedResponse['code']);
+    }
+}
